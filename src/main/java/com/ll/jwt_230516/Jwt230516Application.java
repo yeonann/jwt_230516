53b01d5b234d1f7a09968f86e300e0ea44f9841c
yeonann <yyeonann@gmail.com>
2023-05-16 14:37:09 +0900

    세팅

---
+++ src/main/java/com/ll/jwt_230516/Jwt230516Application.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_230516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt230516Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt230516Application.class, args);
+    }
+
+}
