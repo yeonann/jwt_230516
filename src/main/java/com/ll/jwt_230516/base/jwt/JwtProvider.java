3f91dc4dbea19a0649305e92d6565282229a28a3
yeonann <yyeonann@gmail.com>
2023-05-16 15:20:22 +0900

    토큰 발급

--- src/main/java/com/ll/jwt_230516/base/jwt/JwtProvider.java
+++ src/main/java/com/ll/jwt_230516/base/jwt/JwtProvider.java
@@ -1,26 +1,40 @@
 package com.ll.jwt_230516.base.jwt;
+
+import com.ll.jwt_230516.util.Ut;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtProvider {
     private SecretKey cachedSecretKey;
-
     @Value("${custom.jwt.secretKey}")
     private String secretKeyPlain;
-
     private SecretKey _getSecretKey() {
         String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
         return Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
     }
-
     public SecretKey getSecretKey() {
         if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
 
         return cachedSecretKey;
+    }
+
+    public String genToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Ut.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
     }
 }
--- src/test/java/com/ll/jwt_230516/JwtTests.java
+++ src/test/java/com/ll/jwt_230516/JwtTests.java
@@ -10,16 +10,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtTests {
     @Autowired
     private JwtProvider jwtProvider;
-
     @Value("${custom.jwt.secretKey}")
     private String secretKeyPlain;
-
     @Test
     @DisplayName("secretKey 키가 존재해야한다.")
     void t1() {
@@ -32,18 +33,14 @@ class JwtTests {
         String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
         // Base64 인코딩된 키를 이용하여 SecretKey 객체를 만든다.
         SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
-
         assertThat(secretKey).isNotNull();
     }
-
     @Test
     @DisplayName("JwtProvider 객체로 SecretKey 객체를 생성할 수 있다.")
     void t3() {
         SecretKey secretKey = jwtProvider.getSecretKey();
-
         assertThat(secretKey).isNotNull();
     }
-
     @Test
     @DisplayName("SecretKey 객체는 단 한번만 생성되어야 한다.")
     void t4() {
@@ -51,5 +48,20 @@ class JwtTests {
         SecretKey secretKey2 = jwtProvider.getSecretKey();
 
         assertThat(secretKey1 == secretKey2).isTrue();
+    }
+
+    @Test
+    @DisplayName("accessToken 을 얻는다.")
+    void t5() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+
+        // 지금으로부터 5시간의 유효기간을 가지는 토큰을 생성
+        String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+
+        System.out.println("accessToken : " + accessToken);
+
+        assertThat(accessToken).isNotNull();
     }
 }
