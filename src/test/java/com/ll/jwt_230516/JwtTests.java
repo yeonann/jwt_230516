31f74925da30ecf0900f25039c42631be2a2ac14
yeonann <yyeonann@gmail.com>
2023-05-16 14:44:20 +0900

    시크릿 키

---
+++ src/test/java/com/ll/jwt_230516/JwtTests.java
@@ -0,0 +1,21 @@
+package com.ll.jwt_2023_05_16;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class JwtTests {
+    @Value("${custom.jwt.secretKey}")
+    private String secretKey;
+
+    @Test
+    @DisplayName("secretKey 키가 존재해야한다.")
+    void t1() {
+        assertThat(secretKey).isNotNull();
+    }
+}
